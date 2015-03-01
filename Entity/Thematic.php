3da65f153bdec535b9e44efb00a64b00d2eaf2ed
init-penguin <leonid-repin@levelab.ru>
2015-03-01 21:11:03 +0300

    Removed doctrine/collections dependency

--- Entity/Thematic.php
+++ Entity/Thematic.php
@@ -1,9 +1,7 @@
 <?php
 namespace Levelab\Model\Thematic\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
 use Levelab\Model\Core\Types\Container;
-use Levelab\Model\Core\Types\Content;
 use Levelab\Model\Thematic\Types\ThematicContainer;
 use Levelab\Model\Thematic\Values\ThematicId;
 
@@ -17,10 +15,6 @@ class Thematic implements Container, ThematicContainer {
      * @var ThematicContainer
      */
     private $parent;
-    /**
-     * @var ArrayCollection
-     */
-    private $contents;
 
     /**
      * @param ThematicId $id
@@ -31,7 +25,6 @@ class Thematic implements Container, ThematicContainer {
         $this->id = $id;
         $this->name = $name;
         $this->parent = $parent;
-        $this->contents = new ArrayCollection();
     }
 
     /**
@@ -60,30 +53,6 @@ class Thematic implements Container, ThematicContainer {
      */
     public function getContents() {
         return $this->contents;
-    }
-
-    /**
-     * @param Content $content
-     */
-    public function addContent(Content $content) {
-        $this->contents->add($content);
-    }
-
-    /**
-     * @param Content $content
-     * @throws \Exception
-     */
-    function removeContent(Content $content) {
-        $contentFound = $this->contents->filter(function(Content $existingContent) use($content) {
-            return $existingContent->getId()->compareAgainst($content->getId());
-        })->first();
-
-        if(!$contentFound) {
-            throw new \Exception;
-        }
-        else {
-            $this->contents->removeElement($contentFound);
-        }
     }
 
     /**
